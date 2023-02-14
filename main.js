3df1285aa945d75f609845f15bc6c492a5943824
Zuzanna06 <103110190+Zuzanna06@users.noreply.github.com>
2023-02-14 09:43:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const firstname = "Zuzanna";
+const lastname = "Sich";
+//const age = 26;
+var age = 26;
+console.log(firstname.length);
+
+
+var  my_time = new Date()
+alert(my_time);
+var dt= new Date();
+
+document.write(dt);
+
+
+var myTab = new Array();
+myTab['enfant'] = 'toto';
+myTab['maman'] = 'tati';
+myTab['papa'] = 'tutu';
+
+console.log(myTab);
+
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("ok");
+
+const prenom = ("Zuzanna");
+const nom = "Sich";
+const age = 44;
+const phrase = " Bonjour je m'apelle " + prenom +" "+ nom + " et j'ai " + age + " " + "ans";
+
+alert(phrase)
+
